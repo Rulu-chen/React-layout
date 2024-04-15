5087ed883b5233cd54d00fc76f7ab3b0b0c6fc3b
YUNRU <lu913526@gmail.com>
2024-04-15 16:12:01 +0800

    sass變數管理+遊戲初版

--- pages/game/game-play.js
+++ pages/game/game-play.js
@@ -13,11 +13,19 @@ import {
 export default function GamePlay() {
   return (
     <>
-      <Section>
-        {/* <Image src="/images/layout/icons.png" alt="" width={500} height={500} /> */}
-        <SearchBar />
-        <h1>在這裡放你切好的版,記得就是用兩個Section包住</h1>
-      </Section>
+      {/* <Section> */}
+      {/* <Image src="/images/layout/icons.png" alt="" width={500} height={500} /> */}
+      {/* <SearchBar />
+        <h1>在這裡放你切好的版,記得就是用兩個Section包住</h1> */}
+      <div className="game-play-page">
+        <div className="time-table col-3">AAA</div>
+        <div className="play-div col-9">AAA</div>
+        <div className="chara-group">
+          <div className="character"></div>
+          <div className="talk"></div>
+        </div>
+      </div>
+      {/* </Section> */}
     </>
   )
 }
